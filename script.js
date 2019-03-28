5e1e4934e47d50a06b9784bd48ae6c257a59345a
Eddie-Anguiano <eddiesanguiano@gmail.com>
2019-03-28 11:45:35 -0700

    added isActive class funcion

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var document,
 	console;
 
-var submit = document.querySelector(".submit");
 
-submit.addEventListener("click", function () {
 
-	var ccTips = parseInt(document.getElementById("cc_input").value);
-	var cashTips = parseInt(document.getElementById("cash_input").value);
-	var sbTips = parseInt(document.getElementById("sb_input").value);
+function calculate() {
+
+	var ccTips = parseFloat(document.getElementById("cc_input").textContent);
+	var cashTips = parseFloat(document.getElementById("cash_input").textContent);
+	var sbTips = parseFloat(document.getElementById("sb_input").textContent);
+
 	var totalTips = ccTips + cashTips;
 	var busTips = Math.ceil(totalTips * .1);
 	var barTips = Math.ceil(totalTips * .05);
@@ -23,14 +24,20 @@ submit.addEventListener("click", function () {
 	document.querySelector(".runResult").textContent = `$ ${runTips}`;
 	document.querySelector(".barResult").textContent = `$ ${barTips}`;
 
-
-
-
-
-
-
-
 	console.log(busTips, barTips, runTips, totalTipOut);
+}
 
+function isActive(element) {
+	var userInput = document.getElementsByClassName("user_input");
 
-});
+	for (var i = 0; i < userInput.length; i++) {
+		userInput[i].activeBool = false;
+	}
+
+	for (var i = 0; i < userInput.length; i++) {
+		if (element === userInput[i]) {
+			userInput[i].activeBool = true;
+			userInput[i].parentNode.parentNode.style.borderBottom = "1px solid #39ff14";
+		}
+	}
+}
