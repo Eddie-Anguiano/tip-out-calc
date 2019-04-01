494e7d82d8e0fd9c733c13ec3d0204c410069bc3
Eddie-Anguiano <eddiesanguiano@gmail.com>
2019-03-31 17:57:42 -0700

    added all working calculation fields

--- script.js
+++ script.js
@@ -1,43 +1,157 @@
 var document,
 	console;
+var userInput = document.getElementsByClassName("user_input");
+var ccTipsDiv = document.getElementById("cc_input");
+var cashTipsDiv = document.getElementById("cash_input");
+var sbTipsDiv = document.getElementById("sb_input");
 
 
+for (var i = 0; i < userInput.length; i++) {
+	userInput[i].value = [];
+}
+
+function removeDollarSign(stringNum) {
+
+	var stringArray = stringNum.split("");
+
+	stringArray.shift();
+	stringArray.shift();
+	return stringArray.join("");
+}
 
 function calculate() {
 
-	var ccTips = parseFloat(document.getElementById("cc_input").textContent);
-	var cashTips = parseFloat(document.getElementById("cash_input").textContent);
-	var sbTips = parseFloat(document.getElementById("sb_input").textContent);
+	var ccTips = document.getElementById("cc_input").textContent;
+	var cashTips = document.getElementById("cash_input").textContent;
+	var sbTips = document.getElementById("sb_input").textContent;
 
-	var totalTips = ccTips + cashTips;
+
+	ccTips = parseFloat(removeDollarSign(ccTips));
+	cashTips = parseFloat(removeDollarSign(cashTips));
+	sbTips = parseFloat(removeDollarSign(sbTips));
+
+	var totalTips = ccTips + cashTips - sbTips;
+	var tipsReceived = ccTips + cashTips;
 	var busTips = Math.ceil(totalTips * .1);
-	var barTips = Math.ceil(totalTips * .05);
+	var barTips = Math.ceil(totalTips * .05) + sbTips;
 	var runTips = Math.ceil(totalTips * .05);
 	var totalTipOut = busTips + barTips + runTips;
-	var claimed = totalTips - totalTipOut;
+	var claimed = tipsReceived - totalTipOut;
 
-	document.querySelector(".tipOutResult").textContent = `$ ${totalTipOut}`;
-	document.querySelector(".cashResult").textContent = `$ ${cashTips}`;
-	document.querySelector(".claimedResult").textContent = `$ ${claimed}`;
+	console.log(ccTips, cashTips, sbTips);
 
-	document.querySelector(".busResult").textContent = `$ ${busTips}`;
-	document.querySelector(".runResult").textContent = `$ ${runTips}`;
-	document.querySelector(".barResult").textContent = `$ ${barTips}`;
+	document.querySelector(".tipOutResult").textContent = "$ " + totalTipOut;
+	document.querySelector(".cashResult").textContent = "$ " + cashTips;
+	document.querySelector(".claimedResult").textContent = "$ " + claimed.toFixed(2);
 
-	console.log(busTips, barTips, runTips, totalTipOut);
+	document.querySelector(".busResult").textContent = "$ " + busTips;
+	document.querySelector(".runResult").textContent = "$ " + runTips;
+	document.querySelector(".barResult").textContent = "$ " + barTips;
 }
 
 function isActive(element) {
-	var userInput = document.getElementsByClassName("user_input");
 
 	for (var i = 0; i < userInput.length; i++) {
 		userInput[i].activeBool = false;
 	}
 
-	for (var i = 0; i < userInput.length; i++) {
-		if (element === userInput[i]) {
-			userInput[i].activeBool = true;
-			userInput[i].parentNode.parentNode.style.borderBottom = "1px solid #39ff14";
+	for (var j = 0; j < userInput.length; j++) {
+		if (element === userInput[j]) {
+			userInput[j].activeBool = true;
+			userInput[j].parentNode.parentNode.style.borderBottom = "1px solid #39ff14";
 		}
 	}
+
 }
+
+function dynamicInput(num) {
+
+	for (var i = 0; i < userInput.length; i++) {
+		if (userInput[i].activeBool === true) {
+			unformat(userInput[i].value);
+			userInput[i].value.push(num);
+			formatArray(userInput[i].value);
+		}
+	}
+
+	function unformat(numArray) {
+		var decLocation = numArray.indexOf(".");
+		if (decLocation >= 0) {
+			numArray.splice(decLocation, 1);
+		}
+		if (numArray[0] === 0) {
+			numArray.shift();
+		}
+	}
+
+	function formatArray(array) {
+
+		if (array.length === 1) {
+			array.unshift(0);
+			array.unshift(".");
+		} else if (array.length === 2) {
+			array.unshift(".");
+		} else {
+			array.splice(array.length - 2, 0, ".");
+		}
+	}
+
+}
+
+function buttons() {
+	var numBtns = document.getElementsByClassName("num_Btn");
+	var enterBtn = document.querySelector(".enter_Btn");
+	var clearBtn = document.querySelector(".clear_Btn");
+
+	for (var i = 0; i < numBtns.length; i++) {
+		numBtns[i].addEventListener("click", function (ev) {
+			dynamicInput(ev.target.textContent);
+			for (var i = 0; i < userInput.length; i++) {
+				if (userInput[i].activeBool === true) {
+					var result = userInput[i].value.join("");
+					userInput[i].textContent = "$ " + result;
+				}
+			}
+
+
+		});
+	}
+
+	function enterPress() {
+		enterBtn.addEventListener("click", function () {
+			if (ccTipsDiv.activeBool === true) {
+				isActive(cashTipsDiv);
+			} else if (cashTipsDiv.activeBool === true) {
+				isActive(sbTipsDiv);
+			} else {
+				calculate();
+			}
+		});
+
+
+	}
+
+	function clearPress() {
+		var outVals = document.getElementsByClassName("output_val_area");
+
+		clearBtn.addEventListener("click", function () {
+			for (var i = 0; i < userInput.length; i++) {
+				userInput[i].activeBool = false;
+				userInput[i].value = [];
+				userInput[i].textContent = "$";
+				userInput[i].parentNode.parentNode.style.borderBottom = "1px solid gray";
+			}
+			for (var j = 0; j < outVals.length; j++) {
+				outVals[j].textContent = "$";
+			}
+
+			isActive(ccTipsDiv);
+		})
+	}
+	enterPress();
+	clearPress();
+
+}
+
+buttons();
+isActive(ccTipsDiv);
